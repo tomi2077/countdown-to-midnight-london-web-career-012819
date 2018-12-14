9403f037a4b34a1bac63b364f225e1debffbf72a
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-14 10:20:42 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
+
 #write your code here
 
-def countdown
+def countdown(num)
+  x = 0
+  while x < num
+    puts "#{num} SECOND(S)!"
+    num -= 1
+  end
+return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(int)
+  x = 0
+  while x < int
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep(6)
+  end
+return "HAPPY NEW YEAR!"
+end
+
+
+
+
